5eb4922f3525338c30317e1c1ac746b18b0c89f7
Victor Martinez <vcrmartinez@gmail.com>
2017-07-15 16:51:48 -0300

    Show real estate properties based on db

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from catalog.models import City
+from catalog.models import City, RealEstate
 
 
 class HomePageView(TemplateView):
@@ -9,17 +9,11 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         cities = City.objects.filter(capital=True)
-        context['grouped_cities'] = self.to_groups(cities)
+        real_estates = RealEstate.objects.all()[:6]
+        context['grouped_real_estates'] = self.to_groups(real_estates, 3)
+        context['grouped_cities'] = self.to_groups(cities, 3)
         return context
 
-    def to_groups(self, cities):
-        size = len(cities)
-        if not size:
-            return []
-
-        hop = int(size / 3)
-        return [
-            cities[0:hop],
-            cities[hop: size - hop],
-            cities[hop + hop:]
-        ]
+    def to_groups(self, element_list, number):
+        for i in range(0, len(element_list), number):
+            yield element_list[i:i + number]
