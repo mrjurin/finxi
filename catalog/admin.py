720a19dcddb416c7fb8c6fd07b9dbfa8935dc274
Victor Martinez <vcrmartinez@gmail.com>
2017-07-15 12:59:25 -0300

    Create initial model and admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import RealEstate, Seller, State, Contact, Address, City
 
-# Register your models here.
+
+class RealEstateAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'price', 'sold', 'address', 'created_at')
+
+
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email')
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('code', 'number')
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('street', 'number', 'city', 'postalCode')
+
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'abbreviation')
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'state')
+
+
+admin.site.register(RealEstate, RealEstateAdmin)
+admin.site.register(Seller, SellerAdmin)
+admin.site.register(State, StateAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(City, CityAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,115 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class RealEstate(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    LAND = 'L'
+    HOUSE = 'H'
+    APARTMENT = 'A'
+    FLAT = 'F'
+    PENTHOUSE = 'P'
+    KITNET = 'K'
+    TYPES = (
+        (LAND, 'Terreno'),
+        (HOUSE, 'Casa'),
+        (APARTMENT, 'Apartamento'),
+        (FLAT, 'Flat'),
+        (PENTHOUSE, 'Terreno'),
+        (KITNET, 'Kitnet'),
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.CharField('Tipo', max_length=1, choices=TYPES)
+    area = models.IntegerField('Área')
+    price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    tax = models.DecimalField('IPTU', decimal_places=2, max_digits=8)
+    condoPrice = models.DecimalField('Condomínio', decimal_places=2, null=True, max_digits=8)
+    numberOfBathrooms = models.IntegerField('Banheiros')
+    numberOfRooms = models.IntegerField('Quartos')
+    numberOfSuites = models.IntegerField('Suítes')
+    numberOfCarParks = models.IntegerField('Vagas')
+    sold = models.BooleanField('Vendido', default=False)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    address = models.ForeignKey('catalog.Address', verbose_name='Localização')
+    seller = models.ForeignKey('catalog.Seller', verbose_name='Anunciante')
+
+    class Meta:
+        verbose_name = 'Imóvel'
+        verbose_name_plural = 'Imóveis'
+
+    def __str__(self):
+        return self.title
+
+
+# ToDo: Características: mobiliado, churrasqueira, espaço gourmet, etc..
+# ToDo: Upload de Imagens
+# class Photo(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
+#     path = models.CharField(max_length=255)
+#     default = models.BooleanField(default=False)
+
+
+class Seller(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
+    name = models.CharField('Nome', max_length=100)
+    surname = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('Email')
+
+    class Meta:
+        verbose_name = 'Anunciante'
+        verbose_name_plural = 'Anunciantes'
+
+    def __str__(self):
+        return "{} {}".format(self.name, self.surname)
+
+
+class Contact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
+    code = models.CharField('DDD', max_length=5)
+    number = models.CharField('Número', max_length=20)
+
+    class Meta:
+        verbose_name = 'Telefone'
+        verbose_name_plural = 'Telefones'
+
+    def __str__(self):
+        return "{}-{}".format(self.code, self.number)
+
+
+class Address(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    street = models.CharField('Endereço', max_length=255)
+    number = models.CharField('Número', max_length=5, blank=True, null=True)
+    city = models.ForeignKey('catalog.City', verbose_name='Cidade')
+    postalCode = models.CharField('CEP', max_length=10, blank=True, null=True)
+
+
+class City(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField('Nome', max_length=80)
+    state = models.ForeignKey('catalog.State', verbose_name='Estado')
+    capital = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Cidade'
+        verbose_name_plural = 'Cidades'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class State(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField('Nome', max_length=50)
+    abbreviation = models.CharField('UF', max_length=2, unique=True)
+
+    class Meta:
+        verbose_name = 'Estado'
+        verbose_name_plural = 'Estados'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
