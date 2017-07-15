be3ebe0f2b5bed298dfaab076f043111f33f7c11
Victor Martinez <vcrmartinez@gmail.com>
2017-07-15 13:43:38 -0300

    Get capital cities from db and show in home

--- core/views.py
+++ core/views.py
@@ -1,6 +1,25 @@
 from django.views.generic import TemplateView
 
+from catalog.models import City
+
 
 class HomePageView(TemplateView):
-
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cities = City.objects.filter(capital=True)
+        context['grouped_cities'] = self.to_groups(cities)
+        return context
+
+    def to_groups(self, cities):
+        size = len(cities)
+        if not size:
+            return []
+
+        hop = int(size / 3)
+        return [
+            cities[0:hop],
+            cities[hop: size - hop],
+            cities[hop + hop:]
+        ]
