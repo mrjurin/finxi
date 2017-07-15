e474397bdd694e2bba388d506aa754f2e497d682
Victor Martinez <vcrmartinez@gmail.com>
2017-07-15 20:55:47 -0300

    Implement basic listing view

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from catalog.models import RealEstate
 
 
-class RealEstateListView(TemplateView):
+class RealEstateListView(ListView):
     template_name = 'catalog/real_estate_list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return RealEstate.objects.filter(sold=False)
