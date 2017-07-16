9dbd4a16e7fbfcaad02c865070897be1d728978b
Victor Martinez <vcrmartinez@gmail.com>
2017-07-16 20:46:03 -0300

    Fix list

--- catalog/views.py
+++ catalog/views.py
@@ -7,11 +7,18 @@ class RealEstateListView(ListView):
     template_name = 'catalog/real_estate_list.html'
     paginate_by = 10
 
+
+class RealEstateBuyListView(RealEstateListView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['action'] = 'Comprar'
+        return context
+
     def get_queryset(self):
         city_slug = self.kwargs.get('slug', None)
         state_abbrev = self.kwargs.get('state', None)
 
-        objects = RealEstate.objects.filter(sold=False)
+        objects = RealEstate.objects.filter(sold=False).exclude(transaction_type=RENT)
         if city_slug:
             objects = objects.filter(address__city__slug=city_slug)
 
@@ -20,27 +27,20 @@ class RealEstateListView(ListView):
         return objects
 
 
-class RealEstateBuyListView(RealEstateListView):
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['action'] = 'Comprar'
-        return context
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset.exclude(transaction_type=RENT)
-        return queryset
-
-
 class RealEstateRentListView(RealEstateListView):
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['action'] = 'Alugar'
         return context
 
     def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset.exclude(transaction_type=SELL)
-        return queryset
+        city_slug = self.kwargs.get('slug', None)
+        state_abbrev = self.kwargs.get('state', None)
+
+        objects = RealEstate.objects.filter(sold=False).exclude(transaction_type=SELL)
+        if city_slug:
+            objects = objects.filter(address__city__slug=city_slug)
+
+        if state_abbrev:
+            objects = objects.filter(address__city__state__abbreviation__contains=state_abbrev)
+        return objects
