4e5820fb6e9b75ecc0944e9afa2655635abdbb54
Victor Martinez <vcrmartinez@gmail.com>
2017-07-16 20:36:27 -0300

    Implement real estate show view

--- aluguelimoveis/urls.py
+++ aluguelimoveis/urls.py
@@ -12,23 +12,24 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+
 """
 from django.conf.urls import url
 from django.contrib import admin
 
 from catalog.views import RealEstateBuyListView, RealEstateRentListView
 from core.views import HomePageView
+from realestate.views import ShowRealEstateView
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
+
     url(r'^comprar/(?P<state>[\w]+)/(?P<slug>[\w-]+)/$', RealEstateBuyListView.as_view(), name='list_real_state_buy_param'),
     url(r'^comprar/$', RealEstateBuyListView.as_view(), name='list_real_state_buy'),
 
     url(r'^alugar/(?P<state>[\w]+)/(?P<slug>[\w-]+)/$', RealEstateRentListView.as_view(), name='list_real_state_rent_param'),
     url(r'^alugar/$', RealEstateRentListView.as_view(), name='list_real_state_rent'),
+
+    url(r'^imovel/(?P<real_estate_id>[\w\d-]+)/$', ShowRealEstateView.as_view(), name='show_real_estate'),
     url(r'^admin/', admin.site.urls),
 ]
-
-# url(r'^palestrantes/(?P<slug>[\w-]+)/$', speaker_detail, name='speaker_detail'),
-# /imoveis/comprar/sp/sao-paulo
-# /imoveis/alugar/rs/porto-alegre
--- realestate/views.py
+++ realestate/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from catalog.models import RealEstate
+
+
+class ShowRealEstateView(DetailView):
+    pk_url_kwarg = 'real_estate_id'
+    model = RealEstate
+    context_object_name = 'real_estate'
+    template_name = 'realestate/show.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recommendations'] = self.get_recommendations(context['real_estate'])
+        return context
+
+    def get_recommendations(self, real_estate):
+        city_slug = real_estate.address.city.slug
+        recommendations = RealEstate.objects.filter(address__city__slug=city_slug).exclude(pk=real_estate.pk)[:5]
+
+        if len(recommendations):
+            return recommendations
+
+        state = real_estate.address.city.state
+        return RealEstate.objects.filter(address__city__state=state).exclude(pk=real_estate.pk)[:5]
