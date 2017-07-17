347e0ee7c4402c045a5766c0644b2d8e7b6fcef3
Victor Martinez <vcrmartinez@gmail.com>
2017-07-17 10:48:03 -0300

    Fix register view

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from accounts.views import AccountsLoginView, AccountsLogoutView, RegisterView
 urlpatterns = [
     url(r'^login/$', AccountsLoginView.as_view(), name='login'),
     url(r'^logout/$', AccountsLogoutView.as_view(), name='logout'),
-    url(r'^register/$', RegisterView.as_view(), name='register'),
+    url(r'^registrar/$', RegisterView.as_view(), name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,16 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from .forms import UserAdminCreationForm
 
 User = get_user_model()
 
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
-    template_name = 'accounts/register.html'
     model = User
+    template_name = 'accounts/register.html'
+    form_class = UserAdminCreationForm
     success_url = reverse_lazy('index')
 
 
